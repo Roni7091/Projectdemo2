6698985feafac1535f84ae88859860bab509319e
shubham raj <roni.shubhamraj@gmail.com>
2023-01-06 13:39:24 +0530

    nav update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 
 import './App.css';
-import Ccl from './component2/Ccl';
-import Verification1 from './component2/Verification1';
-import Verification3 from './component2/Verification2';
-import Task4 from './component2/Task4';
-import Verification2 from './component2/Verification2';
-import Task3 from './components/Task3';
+
+import Example from './Practice/Example';
 
 
 function App() {
   return (
     <div className="App">
       
-  <Verification1/>
+  <Verification4/>
 
 
     </div>
---
+++ src/component2/Verification4.js
@@ -0,0 +1,4 @@
+var Number=['20','12','11','25','5','7'];
+var secondlargest = anaumber.sort(function(a,b){return a - b})[Number.length-2];
+alert(secondlargest);
+
